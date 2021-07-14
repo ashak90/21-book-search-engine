ef70e74f39e5b4b1f7935f744af2df7d935eb47a
Asha Kumar <ashakumar90@gmail.com>
2021-07-14 18:47:59 -0400

    added the server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,23 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+const { AppolloServer } = require('apollo-server-express')
+
+const { typeDefs, resolvers } = require('./schemas')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//create a new Apollo server and pass in our schema data
+
+const server = new AppolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +28,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.json(__dirname, '../client/build/index.html'));
+});
+
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
